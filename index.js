8403706455986abc8217393f57a7ed19ef0d6388
Bryan Williams <brywilldev@gmail.com>
2018-02-25 23:58:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length > 0) {
-    return "Currently serving " + katzDeliLine[0] + "."
-  } else {
+  if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
+  } else {
+    return "Currently serving " + katzDeliLine[0] + "."
+    katzDeliLine.shift()
   }
-  katzDeliLine.shift()
 }
