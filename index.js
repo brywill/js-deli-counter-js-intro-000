2006d35f1bd7dff4090d68425f738ef87cd501c4
Bryan Williams <brywilldev@gmail.com>
2018-02-25 23:51:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var katzDeliLine = []
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return katzDeliLine.length + 1
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return katzDeliLine[0]
+    katzDeliLine.shift()
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
